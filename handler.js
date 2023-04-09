b2bcdd518b5f98e225880b6a25ae155f894169a3
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-04-09 16:07:59 +0200

    Support loading preset data

--- handler.js
+++ handler.js
@@ -1,9 +1,7 @@
 const add = triggerGui.getElementsByClassName("add")[0];
 add.addEventListener("click", async () => {
   await new Promise((r) => setTimeout(r, 500));
-  let e = document.createElement("trigger-config");
-  e.classList.add("triggerConfig");
-  triggerGui.appendChild(e);
+  createNewConfig();
 });
 
 getTriggerConfiguration(async () => {
@@ -23,3 +21,22 @@ getTriggerConfiguration(async () => {
     data: configData,
   };
 });
+
+const createNewConfig = () => {
+  let e = document.createElement("trigger-config");
+  e.classList.add("triggerConfig");
+  triggerGui.appendChild(e);
+};
+
+(async () => {
+  if (triggerPresetData) {
+    for (let config of triggerPresetData) {
+      createNewConfig();
+      const triggerConfig = triggerGui.getElementsByClassName("triggerConfig");
+      let elem = triggerConfig[triggerConfig.length - 1];
+      await uiBuilder.ready(elem);
+      await elem.component.prepare();
+      triggerConfig[triggerConfig.length - 1].component.loadConfig(config);
+    }
+  }
+})();
